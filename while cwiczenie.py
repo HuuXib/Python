7c498dfbcbbaa3b3c714c49430edcbbef4d93b71
HubertFronczek <fronczekkhubert@gmail.com>
2022-10-02 22:55:17 +0200

    fajnie

--- while cwiczenie.py
+++ while cwiczenie.py
@@ -1,4 +1,10 @@
-name = input("Podaj swoje imie: ")
+import time
+import traceback
+
+while True:
+    name = input("Podaj swoje imie: ")
+    if name != "":
+        break
 x = int(input("Podaj swój kod dostępu: "))
 
 while x != 5432:
@@ -12,6 +18,15 @@ srodki = 1000
 print("Aktalne środki na koncie wynoszą:",srodki,"PLN")
 z = int(input("Jaką kwotę chcesz wybrać?: "))
 
+confirm = input("Czy na pewno chcesz wybrać podaną kwotę ze swojego konta bankowego TAK/NIE: ")
+if confirm == "TAK":
+    print("Pobieranie danych z serwera... ")
+    time.sleep(4)
+
+elif confirm == "NIE":
+    print("Anulwoano wyciąg z konta")
+    time.sleep(4)
+
 print("----------------------------------------")
 if z <= srodki:
     print("wybrano kwote o wartosci: ", z)
@@ -20,3 +35,6 @@ if z <= srodki:
 
 else:
     print("Zbyt małe środki na koncie. Aktualnie posiadanie środki: ",srodki," PLN")
+
+
+
